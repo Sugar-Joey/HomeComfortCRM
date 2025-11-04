052b4609935ce716e1dfa4cec111da3f3eddb319
Sugar <RachelllKeee@gmail.com>
2025-11-04 22:44:13 +0800

    feat: initialize project with Vue 3 and Element Plus, add Sidebar component and configuration files

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,11 +1,73 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="common-layout">
+    <el-container style="height: 100vh">
+      <!-- 左侧导航栏 -->
+      <!-- <el-aside width="200px" class="aside"> -->
+      <Sidebar />
+      <!-- </el-aside> -->
+
+      <!-- 右侧 -->
+      <el-container>
+        <!-- 右上header -->
+        <el-header class="header">
+          <el-page-header :icon="ArrowLeft">
+            <template #content>
+              <span class="text-large font-600 mr-3"> Title </span>
+            </template>
+          </el-page-header>
+        </el-header>
+
+        <!-- 右下main区域 -->
+        <el-main class="main">
+          <div>You did it!</div>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+import { ArrowLeft } from '@element-plus/icons-vue'
+
+// 使用延迟组件导入来避免静态类型检查对 SFC 默认导出的限制
+const Sidebar = defineAsyncComponent(() => import('./components/Sidebar.vue'))
+
+// 这里将来可以引入 Sidebar、Navbar 组件或控制逻辑
+</script>
+
+<style lang="scss" scoped>
+.aside {
+  background-color: #d3dce6;
+  text-align: center;
+  line-height: 200px;
+  font-weight: bold;
+}
+
+.header {
+  background-color: #409eff;
+  color: white;
+  text-align: center;
+  line-height: 60px;
+  font-size: 20px;
+}
+
+.el-page-header {
+  ::v-deep(.el-page-header__title) {
+    font-size: 20px !important;
+    font-weight: 600;
+  }
+
+  ::v-deep(svg) {
+    width: 20px !important;
+    // height: 20px !important;
+    vertical-align: middle;
+  }
+}
+
+.main {
+  background-color: #e9eef3;
+  padding: 20px;
+  text-align: center;
+}
+</style>
---
+++ front-end/src/components/Sidebar.vue
@@ -0,0 +1,94 @@
+<template>
+  <el-row class="tac">
+    <el-col :span="24">
+      <h5 class="mb-2">Default colors</h5>
+      <!-- 菜单项 -->
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span class="nav">Navigator One</span>
+          </template>
+          <el-menu-item-group title="Group One">
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title>
+              <span class="nav">item four</span>
+            </template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <el-icon><icon-menu /></el-icon>
+          <span>Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <el-icon><document /></el-icon>
+          <span>Navigator Three</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><setting /></el-icon>
+          <span>Navigator Four</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script lang="ts" setup>
+import { Document, Menu as IconMenu, Location, Setting } from '@element-plus/icons-vue'
+
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+// Provide a default export with a multi-word name to satisfy TS and ESLint rules
+export default defineComponent({
+  name: 'AppSidebar',
+})
+</script>
+
+<style scoped>
+/* .aside {
+  background-color: #d3dce6;
+  text-align: center;
+  line-height: 200px;
+  font-weight: bold;
+}
+
+.header {
+  background-color: #409eff;
+  color: white;
+  text-align: center;
+  line-height: 60px;
+  font-size: 20px;
+}
+
+.main {
+  background-color: #e9eef3;
+  padding: 20px;
+  text-align: center;
+} */
+
+.el-menu-item,
+.nav {
+  font-size: 20px;
+}
+</style>
